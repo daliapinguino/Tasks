b219909b97d6d7a7127e84be5201e65d619c5b28
daliapinguino <59578318+daliapinguino@users.noreply.github.com>
2020-05-09 20:52:40 +0300

    Create Michigan.py

---
+++ COVID-19_upgrated/Michigan.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+import pandas as pd
+import datetime
+from selenium.webdriver.chrome.options import Options
+
+time_of_parsing = datetime.datetime.today().strftime('%Y-%m-%d')
+
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Chrome(executable_path='/Users/giova/Documents/Documents/Data Mining/chromedriver',
+                          options=options)
+try:
+
+    driver.get('https://www.michigan.gov/coronavirus/0,9753,7-406-98163-520743--,00.html')
+
+    County_cases = []
+    Death_by_county = []
+    State_cases = []
+    Death_by_state = []
+    County = []
+
+    div_table = driver.find_element_by_class_name('fullContent')
+    info_table = div_table.find_element_by_tag_name('tbody')
+    datas = info_table.find_elements_by_tag_name('tr')
+
+    for info in datas:
+        county_info = info.find_elements_by_tag_name('td')[0].text
+        cases_info = info.find_elements_by_tag_name('td')[1].text
+        death_info = info.find_elements_by_tag_name('td')[2].text
+        # print(county_info, cases_info,death_info)
+        County_cases.append(cases_info)
+        Death_by_county.append(death_info)
+        County.append(county_info)
+
+    driver.quit()
+
+    data = pd.DataFrame({
+        'State': 'Michigan',
+        'County': County,
+        'Cases': County_cases,
+        'Deaths': Death_by_county,
+        'Date': time_of_parsing,
+
+    })
+
+    data.to_csv('Michigan.csv')
+
+except:
+    print('Sorry, data is unavailable, try later... ')
